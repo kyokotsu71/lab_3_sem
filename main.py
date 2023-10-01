c47113367982249e06c261694e850df14ae98385
kyokotsu71 <shchegolevalexei@gmail.com>
2023-10-01 14:00:59 +0200

    add pascal.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("bimbimbambam")
+n = int(input("Введите количество строк треугольника Паскаля: "))
+
+triangle = []
+for i in range(n):
+    row = []
+    for j in range(i+1):
+        if j == 0 or j == i:
+            row.append(1)
+        else:
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+    triangle.append(row)
+
+for row in triangle:
+    print(' '*n, *row, sep=' ')
+    n -= 1
+
