fa8fbf60d6850cb79cd14e7f7b16cd13c6a08160
kyokotsu71 <shchegolevalexei@gmail.com>
2023-10-11 11:48:32 +0200

    labs 4-6

--- ASD/lab 5.py
+++ ASD/lab 5.py
@@ -1,15 +1,13 @@
-def insertion_sort(alist):
-    for i in range(1, len(alist)):
-        temp = alist[i]
-        j = i - 1
-        while j >= 0 and temp < alist[j]:
-            alist[j + 1] = alist[j]
-            j = j - 1
-        alist[j + 1] = temp
-
-
+# сортировка вставками
 alist = input('Enter the list of numbers: ').split()
 alist = [int(x) for x in alist]
-insertion_sort(alist)
+for i in range(1, len(alist)):
+    temp = alist[i]
+    j = i - 1
+    while j >= 0 and temp < alist[j]:
+        alist[j + 1] = alist[j]
+        j = j - 1
+    alist[j + 1] = temp
+
 print('Sorted list: ', end='')
 print(alist)
